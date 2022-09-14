0b31d5d9ef84849db842866b4b14e6cbc8c6ec61
MightySarabor <fleschm@hochschule-trier.de>
2022-09-14 14:40:30 +0200

    changed Schmi to Fleschm

--- src/main/java/de/hstr/bigdata/Pipe.java
+++ src/main/java/de/hstr/bigdata/Pipe.java
@@ -20,10 +20,10 @@ public class Pipe {
 
         System.err.println("Pipe.java");
         
-        System.setProperty("java.security.auth.login.config", "/home/schmi/kafka.jaas");
+        System.setProperty("java.security.auth.login.config", "/home/fleschm/kafka.jaas");
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "schmi-test");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "fleschm-test");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
                 "infbdt07.fh-trier.de:6667,infbdt08.fh-trier.de:6667,infbdt09.fh-trier.de:6667");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -37,9 +37,9 @@ public class Pipe {
 
         System.err.println("-----Starting Processor-----");
         
-        builder.stream("schmi-1")
+        builder.stream("fleschm-1")
                 .peek((k, v) -> { System.err.println("Peek: " + v);})
-                .to("schmi-2");
+                .to("fleschm-2");
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
