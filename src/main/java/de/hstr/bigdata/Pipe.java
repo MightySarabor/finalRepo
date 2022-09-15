d09f99faae393b97a5b11e2d80ab0597f7e04e0c
MightySarabor <fleschm@hochschule-trier.de>
2022-09-15 16:56:40 +0200

    changed name for new intern topics

--- src/main/java/de/hstr/bigdata/Pipe.java
+++ src/main/java/de/hstr/bigdata/Pipe.java
@@ -32,7 +32,7 @@ public class Pipe {
         System.setProperty("java.security.auth.login.config", "/home/fleschm/kafka.jaas");
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "fleschm-test");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "fleschm-final-pizza");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
                 "infbdt07.fh-trier.de:6667,infbdt08.fh-trier.de:6667,infbdt09.fh-trier.de:6667");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
