e84cf670b06d14348911899618f45f9fd8916e83
MightySarabor <fleschm@hochschule-trier.de>
2022-09-16 09:45:26 +0200

    changed to cluster

--- src/main/java/de/hstr/bigdata/Pipe.java
+++ src/main/java/de/hstr/bigdata/Pipe.java
@@ -28,7 +28,7 @@ public class Pipe {
 
         System.err.println("Pipe.java");
         
-        //System.setProperty("java.security.auth.login.config", "/home/fleschm/kafka.jaas");
+        System.setProperty("java.security.auth.login.config", "/home/fleschm/kafka.jaas");
 
         Properties props = new Properties();
         //props.put(StreamsConfig.APPLICATION_ID_CONFIG, "fleschm-final-pizzza");
@@ -38,9 +38,9 @@ public class Pipe {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
 
-        //props.put("security.protocol", "SASL_PLAINTEXT");
-        //props.put("enable.auto.commit", "true");
-        //props.put("auto.commit.interval.ms", "1000");
+        props.put("security.protocol", "SASL_PLAINTEXT");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
         if (args.length < 1) {
             throw new IllegalArgumentException("This program takes one argument: the path to a configuration file.");
         }
