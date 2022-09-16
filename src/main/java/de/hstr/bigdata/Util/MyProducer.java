0b9237baa49bff83f2da75dc1782dec8dbec7a55
MightySarabor <fleschm@hochschule-trier.de>
2022-09-16 10:17:41 +0200

    asds

--- src/main/java/de/hstr/bigdata/Util/MyProducer.java
+++ src/main/java/de/hstr/bigdata/Util/MyProducer.java
@@ -43,6 +43,7 @@ public class MyProducer {
 
             System.err.println(value);
             first_producer.send(record);
+            System.err.println("Message sent");
             first_producer.flush();
     }
 }
