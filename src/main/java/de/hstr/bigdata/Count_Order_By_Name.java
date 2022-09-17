595c12684290bc693346104670307b7e9fb7cfce
MightySarabor <fleschm@hochschule-trier.de>
2022-09-17 23:44:18 +0200

    removed first peek2

--- src/main/java/de/hstr/bigdata/Count_Order_By_Name.java
+++ src/main/java/de/hstr/bigdata/Count_Order_By_Name.java
@@ -98,8 +98,8 @@ public class Count_Order_By_Name {
         final StreamsBuilder builder = new StreamsBuilder();
 
         final KStream<String, OrderPOJO> orderStream =
-                builder.stream(inputTopic, Consumed.with(Serdes.String(), new JSONSerde()))
-                        .peek((key, value) -> System.out.println("value " + value));
+                builder.stream(inputTopic, Consumed.with(Serdes.String(), new JSONSerde()));
+                        //.peek((key, value) -> System.out.println("value " + value));
         orderStream
                 .groupByKey()
                 .windowedBy(TimeWindows.ofSizeAndGrace(Duration.ofMinutes(1), Duration.ofSeconds(15)))
@@ -123,8 +123,8 @@ public class Count_Order_By_Name {
 
         List customers = POJOGenerator.generateCustomer(NUMBER_OF_CUSTOMERS);
 
-        ScheduledExecutorService exec = Executors.newScheduledThreadPool(2);
-        exec.scheduleAtFixedRate(() -> MyProducer.produceOrder(customers, false), 10, 60, TimeUnit.SECONDS);
+        ScheduledExecutorService exec = Executors.newScheduledThreadPool(10);
+        exec.scheduleAtFixedRate(() -> MyProducer.produceOrder(customers, false), 1, 1, TimeUnit.SECONDS);
         Properties props = setProps(false);
 
         KafkaStreams kafkaStreams = new KafkaStreams(
