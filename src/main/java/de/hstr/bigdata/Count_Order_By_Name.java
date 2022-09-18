cc69111b48788f4836242fb026e0442e55866ea9
MightySarabor <fleschm@hochschule-trier.de>
2022-09-18 15:19:32 +0200

    changed replication to 1 from

--- src/main/java/de/hstr/bigdata/Count_Order_By_Name.java
+++ src/main/java/de/hstr/bigdata/Count_Order_By_Name.java
@@ -65,7 +65,7 @@ public class Count_Order_By_Name {
                 props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
                 props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
                 props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-                props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 2);
+                props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
 
             }
             return props;
