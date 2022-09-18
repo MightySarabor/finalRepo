2964527c39845b9b42d421281a8c6069aec7bc4d
MightySarabor <fleschm@hochschule-trier.de>
2022-09-18 15:29:20 +0200

    changed to cluster

--- src/main/java/de/hstr/bigdata/Count_Order_By_Name.java
+++ src/main/java/de/hstr/bigdata/Count_Order_By_Name.java
@@ -134,8 +134,8 @@ public class Count_Order_By_Name {
         List customers = POJOGenerator.generateCustomer(NUMBER_OF_CUSTOMERS);
 
         ScheduledExecutorService exec = Executors.newScheduledThreadPool(10);
-        exec.scheduleAtFixedRate(() -> MyProducer.produceOrder(customers, false), 1, 1, TimeUnit.SECONDS);
-        Properties props = setProps(false);
+        exec.scheduleAtFixedRate(() -> MyProducer.produceOrder(customers, true), 1, 1, TimeUnit.SECONDS);
+        Properties props = setProps(true);
 
         KafkaStreams kafkaStreams = new KafkaStreams(
                 countCustomerInWindow("fleschm-final-order", "fleschm-2"),
