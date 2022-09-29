c40239d198c52a671068345a039f52f04343cb5f
MightySarabor <100364387+MightySarabor@users.noreply.github.com>
2022-09-29 12:35:39 +0200

    Update MyProducer.java

--- src/main/java/de/hstr/bigdata/Util/MyProducer.java
+++ src/main/java/de/hstr/bigdata/Util/MyProducer.java
@@ -83,7 +83,7 @@ public class MyProducer {
         for(int i = 0; i < 5; i++){
             prod[i] = new MyProducer(200000*i, 200000* (i+1));
             int finalI = i;
-            exec.scheduleAtFixedRate(() -> prod[finalI].produceOrder(args[0]), 100, 100, TimeUnit.MILLISECONDS);
+            exec.scheduleAtFixedRate(() -> prod[finalI].produceOrder(args[0]), 10, 10, TimeUnit.MILLISECONDS);
         }
 
     }
